7edbfb02816fbd792ef86a3f830a797f66935f71
swan <tndhks0708@naver.com>
2025-05-15 22:54:15 +0900

    마이페이지: 이름, 로그아웃, 회원탈퇴

--- src/pages/MyPage/components/DeleteAccount.jsx
+++ src/pages/MyPage/components/DeleteAccount.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function DeleteAccount() {
-    return <div>회원 탈퇴 페이지입니다.</div>;
+    const navigate = useNavigate();
+    const hasConfirmed = useRef(false); // 리렌더링에도 값 유지
+
+    useEffect(() => {
+        if (!hasConfirmed.current) {
+            const confirmed = window.confirm('정말로 회원 탈퇴하시겠습니까?');
+
+            if (confirmed) {
+                localStorage.removeItem('accessToken');
+                navigate('/login');
+            } else {
+                navigate('/mypage');
+            }
+
+            hasConfirmed.current = true; // 한 번만 실행
+        }
+    }, [navigate]);
+
+    return <div>회원 탈퇴 처리 중...</div>;
 }
--- src/pages/MyPage/components/Logout.jsx
+++ src/pages/MyPage/components/Logout.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Logout() {
-    return <div>로그아웃 처리 중입니다.</div>;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        localStorage.removeItem('accessToken');
+
+        navigate('/login');
+    }, [navigate]);
+
+    return <div>로그아웃 처리 중...</div>;
 }
