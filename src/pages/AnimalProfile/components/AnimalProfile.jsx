29ee60f5e643a67f0392813173cc862cd89937c4
swan <tndhks0708@naver.com>
2025-05-17 03:29:58 +0900

    동물프로필:navigate, title 변경

--- src/pages/AnimalProfile/components/AnimalProfile.jsx
+++ src/pages/AnimalProfile/components/AnimalProfile.jsx
@@ -508,7 +508,7 @@ export default function AnimalProfile() {
         console.log('📸 previewUrl:', previewUrl);
     }, [previewUrl]);
     const goBack = () => {
-        navigate('/mapmain');
+        navigate('/main');
     };
     return (
         <div className="animal-profile">
@@ -516,7 +516,7 @@ export default function AnimalProfile() {
                 <div className="back-button2" onClick={goBack}>
                     <IoIosArrowBack size={32} />
                 </div>
-                <div className="filtering-title">필터링</div>
+                <div className="filtering-title">반려견 정보 입력</div>
             </div>
 
             <div className="animal-profile-content">
